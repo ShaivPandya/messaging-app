e8318d641be47c36e760b95a2fa125914eee72a8
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2021-11-06 17:28:36 -0700

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Shaiv Pandya on 11/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
