2c458078c01c7818730b17ac7950bec2773a7d91
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2021-11-10 18:47:56 -0800

    Fix app always showing conversation view
    The function viewDidAppear() did not call validateAuth() so the
    app always displayed the conversation view even if there was no
    user logged in.

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -18,7 +18,7 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+        validateAuth()
     }
     
     private func validateAuth() {
