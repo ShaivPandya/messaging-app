d77ccbb5da06be623a834c8b56d8b1f37d3efa44
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2021-11-16 14:23:33 -0800

    Sending messages and storing them in Firebase

--- MessagingApp/Controllers/ConversationsViewController.swift
+++ MessagingApp/Controllers/ConversationsViewController.swift
@@ -12,7 +12,7 @@ import JGProgressHUD
 class ConversationsViewController: UIViewController {
     
     private let spinner = JGProgressHUD(style: .dark)
-
+    
     private let tableView: UITableView = {
         let table = UITableView()
         table.isHidden = true
@@ -43,15 +43,30 @@ class ConversationsViewController: UIViewController {
     
     @objc private func didTapComposeButton() {
         let vc = NewConversationViewController()
+        vc.completion = { [weak self] result in
+            self?.createNewConversation(result: result)
+        }
         let navVC = UINavigationController(rootViewController: vc)
         present(navVC, animated: true)
+    }
+    
+    private func createNewConversation(result: [String: String]) {
+        guard let name = result["name"], let email = result["email"] else {
+            return
+        }
+        
+        let vc = ChatViewController(with: email)
+        vc.isNewConversation = true
+        vc.title = name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         validateAuth()
@@ -90,7 +105,8 @@ extension ConversationsViewController: UITableViewDelegate, UITableViewDataSourc
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let vc = ChatViewController()
+        
+        let vc = ChatViewController(with: "janedoe@gmail.com")
         vc.title = "Jane Doe"
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
